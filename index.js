ab6ba67fb397247322af60006cd55bb701316c3f
Sebastien <sebdh@hotmail.com>
2023-07-08 22:03:58 -0600

    Changed server to hold 1 source of truth. videoList coming from video-details.json now

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ const fs = require("fs");
 const port = 5050;
 const cors = require("cors");
 
+app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  const videoList = JSON.parse(fs.readFileSync("./data/videos.json"));
+app.get("/videos", (req, res) => {
+  const videoList = JSON.parse(fs.readFileSync("./data/video-details.json"));
   res.status(200).json(videoList);
+});
+
+app.get("/videos/:id", (req, res) => {
+  const videoList = JSON.parse(fs.readFileSync("./data/video-details.json"));
+
+  const selectedVideo = videoList.find((video) => {
+    return (video.id === req.params.id);
+  });
+  res.status(200).json(selectedVideo);
 });
 
 app.listen(port, () => {
