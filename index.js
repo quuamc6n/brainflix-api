0a53031c1ad43888dbfca7fd733358c1c02b9d70
Sebastien <sebdh@hotmail.com>
2023-07-10 00:34:18 -0600

    Added functinality for writing and re-reading from JSON file for front end to collect newest upload information

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const fs = require("fs");
 const port = 5050;
 const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
 
 app.use(express.json());
 app.use(cors());
@@ -20,6 +22,57 @@ app.get("/videos/:id", (req, res) => {
   });
   res.status(200).json(selectedVideo);
 });
+
+app.post("/videos", (req, res) => {
+  const { title, description } = req.body;
+  const id = uuidv4();
+
+  const uploadData = {
+    id,
+    title,
+    description,
+    image: "http://localhost:5050/images/brainflix_static.png",
+    channel: "Cat's Eyes",
+    views: 101010,
+    likes: 101010,
+    timestamp: Date(),
+    comments: [
+      {
+        id: "c93c16f0-4795-45d1-b0da-21696d54f25aa",
+        name: "Fionna Miller",
+        comment:
+          "I added an a to the end of each of these comments' IDs. I am also hard coding these comments just to make them different. However, I am keeping the names and times to avoid any errors :)",
+        likes: 6,
+        timestamp: 1631816492000,
+      },
+      {
+        id: "99938bd4-67f9-4404-ad3e-b23a6ad05717a",
+        name: "Suzie Maxwell",
+        comment:
+          "I added an a to the end of each of these comments' IDs. I am also hard coding these comments just to make them different. However, I am keeping the names and times to avoid any errors :)",
+        likes: 1,
+        timestamp: 1631799181000,
+      },
+      {
+        id: "fc2e9a8c-7daa-4e14-980d-5467ca2054eca",
+        name: "Alasie Rivers",
+        comment:
+          "I added an a to the end of each of these comments' IDs. I am also hard coding these comments just to make them different. However, I am keeping the names and times to avoid any errors :)",
+        likes: 0,
+        timestamp: 1631716921000,
+      },
+    ],
+  };
+
+  const currentData = JSON.parse(fs.readFileSync("./data/video-details.json"));
+  currentData.push(uploadData);
+
+  fs.writeFileSync("./data/video-details.json", JSON.stringify(currentData));
+
+  res.sendStatus(200);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
