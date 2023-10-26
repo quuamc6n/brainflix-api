5469c9e6deafbb6041beed5c01ec653136f4757b
quuamc6n <63206589+quuamc6n@users.noreply.github.com>
2023-10-26 16:26:13 -0600

    added env file for hosting purposes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const fs = require("fs");
-const port = 5050;
+const port = process.env.PORT || 5050;
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
 const path = require("path");
-
 app.use(express.json());
 app.use(cors());
 
